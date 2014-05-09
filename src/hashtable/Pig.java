40aee5b72e3484d72176c93b2de3efc5aa1b9868
Trevin Hofmann <trevinhofmann@gmail.com>
2014-05-09 09:01:06 -0500

    First commit

---
+++ src/hashtable/Pig.java
@@ -0,0 +1,21 @@
+package hashtable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hofmannt
+ * Date: 5/9/14
+ * Time: 8:36 AM
+ * This is a very useful class.
+ */
+
+public class Pig {
+
+	/**
+	 * Determines whether or not pigs can fly, and returns the result
+	 * @return boolean canFly
+	 */
+	public static boolean canFly(){
+		return false;
+	}
+
+}
