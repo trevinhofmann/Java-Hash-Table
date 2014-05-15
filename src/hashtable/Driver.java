8a9f495df25006d453c5cb09efcd43ed3c0d1d5d
Trevin Hofmann <trevinhofmann@gmail.com>
2014-05-14 19:59:54 -0500

    test method and result printing

--- src/hashtable/Driver.java
+++ src/hashtable/Driver.java
@@ -17,10 +17,16 @@ public class Driver {
 	}
 
 	private static void test(String filename, int capacity){
-
+		HashTable<String> hashTable = loadHashTable(filename, capacity);
+		int size = hashTable.size();
+		double loadFactor = hashTable.loadFactor();
+		int numberOfCollisions = hashTable.numberOfCollisions();
+		int biggestBucket = hashTable.biggestBucket();
+		int numberOfEmptyBuckets = hashTable.numberOfEmptyBuckets();
+		printResults(filename, capacity, size, loadFactor, numberOfCollisions, biggestBucket, numberOfEmptyBuckets);
 	}
 
-	private HashTable loadHashTable(String filename, int capacity){
+	private static HashTable loadHashTable(String filename, int capacity){
 		HashTable<String> hashTable = new HashTable<String>(capacity);
 		Scanner scanner = null;
 		try{
@@ -39,6 +45,35 @@ public class Driver {
 			}
 		}
 		return hashTable;
+	}
+
+	/**
+	 * Prints the heading labels for result output to the console
+	 */
+	private static void printLabels(){
+		System.out.printf("| %15s | %15s | %15s | %25s | %25s | %25s | %25s |%n",
+				"File",
+				"Capacity",
+				"Size",
+				"Load Factor",
+				"Number of Collisions",
+				"Size of Biggest Bucket",
+				"Number of Empty Buckets");
+	}
+
+	/**
+	 * Prints the heading labels for result output to the console
+	 */
+	private static void printResults(String filename, int capacity, int size, double loadFactor,
+									 int numberOfCollisions, int biggestBucket, int numberOfEmptyBuckets){
+		System.out.printf("| %15s | %15s | %15s | %25s | %25s | %25s | %25s |%n",
+				filename,
+				capacity,
+				size,
+				loadFactor,
+				numberOfCollisions,
+				biggestBucket,
+				numberOfEmptyBuckets);
 	}
 
 }
