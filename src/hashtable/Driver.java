11e2c9993629454b7ed57f1b63d06aac73af4079
Trevin Hofmann <trevinhofmann@gmail.com>
2014-05-14 19:50:22 -0500

    loadHashTable

--- src/hashtable/Driver.java
+++ src/hashtable/Driver.java
@@ -1,5 +1,7 @@
 package hashtable;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hofmannt
@@ -12,6 +14,31 @@ public class Driver {
 
 	public static void main(String[] args){
 
+	}
+
+	private static void test(String filename, int capacity){
+
+	}
+
+	private HashTable loadHashTable(String filename, int capacity){
+		HashTable<String> hashTable = new HashTable<String>(capacity);
+		Scanner scanner = null;
+		try{
+			scanner = new Scanner(filename);
+			String line;
+			while (scanner.hasNextLine()){
+				line = scanner.nextLine();
+				for (String word : line.replaceAll("[^a-zA-Z' -]", "").toLowerCase().split(" ")){
+					hashTable.add(word);
+				}
+			}
+		}
+		finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+		return hashTable;
 	}
 
 }
