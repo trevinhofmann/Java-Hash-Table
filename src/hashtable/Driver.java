7b7f5d6da151a8e699ec565247e189795b6477fd
Trevin Hofmann <trevinhofmann@gmail.com>
2014-05-14 19:41:48 -0500

    create Driver class

---
+++ src/hashtable/Driver.java
@@ -0,0 +1,17 @@
+package hashtable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hofmannt
+ * Date: 5/14/14
+ * Time: 7:36 PM
+ * This is the Driver class for testing the HashTable.
+ */
+
+public class Driver {
+
+	public static void main(String[] args){
+
+	}
+
+}
